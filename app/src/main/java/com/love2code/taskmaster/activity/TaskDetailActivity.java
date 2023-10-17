7dedfb6d960a85c6ae2a2dda1de3355c44a0c8be
Mohamad Samara <mohamadsamara1211@gmail.com>
2023-10-18 02:45:28 +0300

    lab27

---
+++ app/src/main/java/com/love2code/taskmaster/activity/TaskDetailActivity.java
@@ -0,0 +1,34 @@
+package com.love2code.taskmaster.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.love2code.taskmaster.R;
+
+public class TaskDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_detail);
+
+        Intent callingIntent = getIntent();
+        String taskNameString = null;
+
+        if(callingIntent != null){
+            taskNameString = callingIntent.getStringExtra(MainActivity.TASK_NAME_TAG);
+
+            TextView taskTextView = findViewById(R.id.taskDetailTxt);
+
+            if (taskNameString != null){
+                taskTextView.setText(taskNameString);
+            }else {
+                taskTextView.setText("Task Unknown");
+            }
+
+        }
+    }
+}
